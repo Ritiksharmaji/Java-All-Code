e7ca0c29962698a0292fd38f8ac40b8ef8744002
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-02-14 14:01:15 +0530

    adding the package

---
+++ Java-Core-DS-Code/JavaCoreCodeWithDS/JavaOOpsProgram/src/javaOops/_6_Packages/package-info.java
@@ -0,0 +1,1 @@
+package javaOops._6_Packages;
---
+++ Java-Core-DS-Code/JavaCoreCodeWithDS/JavaOOpsProgram/src/javaOops/_6_Packages/package_1_Built_in.java
@@ -0,0 +1,15 @@
+package javaOops._6_Packages;
+
+import java.util.ArrayList;
+
+public class package_1_Built_in {
+	
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Packages");
+        System.out.println(list);
+    }
+
+
+}
